a83ac8f002dfa813e68bda8d6fb274566a2b42c6
santi <santimezquita@hotmail.es>
2023-02-01 17:17:14 +0100

    demo version 1.0

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
-const inst = document.querySelector('#instructions');
+const instButtons = document.querySelectorAll('.instructions');
 const popInst = document.querySelector('#pop-inst');
 const playOptions = document.querySelectorAll('.options-container>div')
-inst.addEventListener('click', ()=> popInst.classList.toggle('invisible'));
+const computerOptions = document.querySelectorAll(`.computer-options>div`);
+const computerDecision = () => Math.floor(Math.random() * 3);
+
+instButtons.forEach(button=>button.addEventListener('click', ()=> popInst.classList.toggle('hideInstructions')));
 function userOption(e) {
     console.log(e.target.id)
+    let computerBet = computerDecision();
+    computerOptions.forEach(option => option.id !== computerBet.toString()? option.classList.toggle('invisible'):null)
+    playOptions.forEach(option => option.id !== e.target.id? option.classList.toggle('invisible'):null)
 }
-playOptions.forEach(div=>div.addEventListener('click',userOption))
+playOptions.forEach(div=>div.addEventListener('click',userOption));
